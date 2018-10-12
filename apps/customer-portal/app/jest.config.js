016a02bd383a46c7b3fdb622a1363260bbabdde8
Jonathan Nungaray <jnungaray@us.costar.local>
2018-10-12 16:23:00 -0700

    First Commit

---
+++ apps/customer-portal/app/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'customer-portal-app',
+  preset: '../../../jest.config.js',
+  coverageDirectory: '../../../coverage/apps/customer-portal/app/',
+  moduleFileExtensions: ["js", "jsx", "json", "ts", "tsx"]
+};
+
